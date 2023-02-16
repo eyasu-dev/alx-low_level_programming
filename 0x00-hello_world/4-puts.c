b984afadf35431742684ac8a0e40787b88aed851
eyasu <eyasusintayehu01@gmail.com>
2023-02-16 10:19:20 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,17 +2,11 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual 
- * puzzle, followed by a newline.
- * Return: Always 0.
+ * main - A program that prints a line with puts function
+ * Return: 0 (Success)
  */
-
-
 int main(void)
-
-
 {
 	puts("\"programming is like building a multilingual puzzle");
-
 	return (0);
 }
