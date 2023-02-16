ae642f2f1d6ac349d679826b0385b25fb6a09344
eyasu <eyasusintayehu01@gmail.com>
2023-02-16 08:46:59 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,14 @@
 
 
 /**
- * main - prints "Programming is like building a multilingual puzzle,
- * * followed by a newline.
+ * main - prints "Programming is like building a multilingual 
+ * puzzle, followed by a newline.
  * Return: Always 0.
  */
 
+
 int main(void)
+
 
 {
 	puts("\"programming is like building a multilingual puzzle");
