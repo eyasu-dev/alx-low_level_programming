360f17c1c0b5ea0d00487369cda4318d9563eb55
eyasu <eyasusintayehu01@gmail.com>
2023-02-17 02:08:24 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,17 @@
 
 
 /**
- * main - A program that prints a line with puts function
- * Retrun: 0 (Success)
- * return value of notign
+ * main - puts print"A program that prints a line with puts function".
+ *
+ * Retrun: Always 0.
  */
 
 int main(void)
 
 {
+
 puts("\"Programming is like building a multilingual puzzle");
+
 return (0);
 }
 
