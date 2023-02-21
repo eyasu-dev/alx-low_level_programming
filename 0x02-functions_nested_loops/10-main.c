7b67e3f9a85801cb91f37d823be62c82b9f2bfcd
eyasu <eyasusintayehu01@gmail.com>
2023-02-21 12:05:35 -0800

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * add - is the num.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
