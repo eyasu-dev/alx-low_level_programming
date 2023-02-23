feda626ec6bb058d476cafea446f71669d4759e8
eyasu <eyasusintayehu01@gmail.com>
2023-02-23 05:17:43 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9 followed by a new line.
+ *
+ * Return: returns
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
