79874b01b66e5d48f3d767c7fcfc584c04f6cff4
eyasu <eyasusintayehu01@gmail.com>
2023-03-11 07:40:06 -0800

    i did it on my own

---
+++ test_statics/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+int add(int a, int b);
+int sub(int a, int b);
+
+#endif
---
+++ test_statics/result.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("add two number, add(40, 2) = %d ", add(40, 2));
+	printf("\nsub to number, 40 - 2 is  =  %d \n", sub(40, 2));
+
+	return 0;
+}
